03de7bf40e24609ab352d2665b5d8f48a03192a3
innxrmxst <43517528+innxrmxst@users.noreply.github.com>
2023-02-16 11:37:49 +0100

    Update users.py

--- users.py
+++ users.py
@@ -13,7 +13,6 @@ with open(bhuserfile) as data_file:
     data = json.load(data_file)
 
 print(data['data'][0]['Properties'])
-#print("name,enabled,displayname,email,title,description")
 for i in data['data']:
     oname = i['Properties']['name'] 
     print("{}".format(oname))
